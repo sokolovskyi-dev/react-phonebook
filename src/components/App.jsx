5fd27885aca06a1b09e513fc65d51475c2912603
DevMasterX <sokol080288@gmail.com>
2025-09-18 01:14:12 +0300

    phonebook start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,101 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+// export const App = () => {
+//   return (
+//     <div>
+//       <h1>Phonebook</h1>
+//       <form>
+//         <input
+//           type="tel"
+//           name="number"
+//           pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+//           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+//           required
+//         />
+//         <button type="submit">Add contact</button>
+//       </form>
+//     </div>
+//   );
+// };
+import { nanoid } from 'nanoid';
+import { Component } from 'react';
+
+export class App extends Component {
+  state = {
+    contacts: [
+      { id: 1, name: 'Rosie Simpson', number: '645-17-49' },
+      { id: 2, name: 'Hermonie Kline', number: '555-22-41' },
+      { id: 3, name: 'Eden Clements', number: '555-13-94' },
+    ],
+    name: '',
+    number: '',
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    this.setState(prevState => ({
+      contacts: [
+        ...prevState.contacts,
+        { id: nanoid(), name: this.state.name, number: this.state.number },
+      ],
+    }));
+    this.reset();
+  };
+
+  reset = () => {
+    this.setState({ name: '', number: '' });
+  };
+
+  handleChange = e => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+    // console.log(e.target.name);
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Name
+            <input
+              onChange={this.handleChange}
+              value={this.state.name}
+              type="text"
+              name="name"
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+            />
+          </label>
+
+          <label>
+            Number
+            <input
+              onChange={this.handleChange}
+              value={this.state.number}
+              type="tel"
+              name="number"
+              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+              required
+            />
+          </label>
+
+          <button type="submit">Add contact</button>
+        </form>
+
+        <h2>Contacts</h2>
+        <ul>
+          {this.state.contacts.map(({ id, name, number }) => {
+            return (
+              <li key={id}>
+                {name}: {number}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
+}
